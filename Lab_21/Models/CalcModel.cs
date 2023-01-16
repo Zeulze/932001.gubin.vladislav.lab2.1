bd095665c6498ced68314b325d1466b447e8d4d5
Vladislav Gubin <49865020+Zeulze@users.noreply.github.com>
2023-01-16 23:42:34 +0700

    Add files via upload

---
+++ Lab_21/Models/CalcModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab21.Models {
+    public class CalcModel 
+    {
+        public readonly int X;
+        public readonly int Y;
+        public readonly string Sum;
+        public readonly string Min;
+        public readonly string Mult;
+        public readonly string Div;
+        public CalcModel(int x, int y, string sum, string min, string mult, string div) {
+            X = x;
+            Y = y;
+            Sum = sum;
+            Min = min;
+            Mult = mult;
+            Div = div;
+        }
+    }
+}
